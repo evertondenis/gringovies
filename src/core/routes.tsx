16a48bf5e9810ee1fc6aa23e59be06d190473d28
Everton Denis <evertondenis@gmail.com>
2021-02-19 13:17:36 -0300

    fix routers

--- src/core/routes.tsx
+++ src/core/routes.tsx
@@ -6,19 +6,19 @@ import Home from 'containers/Main'
 import Movie from 'containers/Movie'
 import FavoritesMovies from 'containers/Favorites'
 import Watchlist from 'containers/Watchlist'
-import SearchPage from 'containers/Search'
+// import SearchPage from 'containers/Search'
 
 export const Routes = () => {
   return (
     <>
       <Header />
       <Switch>
-        <Route path="/:page?/:query?" exact component={Home} />
         <Route path="/movie/:id" exact component={Movie} />
         <Route path="/favorites" exact component={FavoritesMovies} />
         <Route path="/watchlist" exact component={Watchlist} />
+        <Route path="/:page?/:query?" exact component={Home} />
         {/* <Route path="/search/:query" exact component={SearchPage} /> */}
-        <Route path="*" exact component={Home} />
+        {/* <Route path="*" exact component={Home} /> */}
       </Switch>
     </>
   )
