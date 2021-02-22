c61f709ea50001bfac20234cdc6d4afbad764a3e
Everton Denis <evertondenis@gmail.com>
2021-02-22 00:08:59 -0300

    config absolute paths for jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
 module.exports = {
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src')]
 }
